988099eb3e9c9e9f73ad18f19bab15b460493fe8
jaredbawden <64454890+jaredbawden@users.noreply.github.com>
2020-04-28 09:19:39 -0600

    Add files via upload

---
+++ hashmap.py
@@ -0,0 +1,85 @@
+'''
+Jared Bawden
+Project 7 - Dictionaries
+'''
+
+class HashMap:
+    ''' Uses LInear Probe to store key-value pairs '''
+    INITIAL_CAPACITY = 8
+    max_loadfactor = .8
+    def __init__(self, initial_capacity=INITIAL_CAPACITY):
+        ''' creates an empty HashMap'''
+        self.lyst = [None] * initial_capacity
+        self.num_keys = 0
+
+    def get(self, key, default=None):
+        ''' returns value of the key '''
+        sum = 0
+        for letter in key:
+            sum += ord(letter)
+        index = sum % self.capacity()
+        while self.lyst[index] is not None:
+            if self.lyst[index][0] == key:
+                return self.lyst[index][1]
+            index += 1
+            if index == len(self.lyst):
+                index -= len(self.lyst)
+        return default
+
+    def set(self, key, value):
+        ''' adds the key-value pair to the hashmap. If load-factor is greater than 80%, rehash the map '''
+        sum = 0
+        for letter in key:
+            sum += ord(letter)
+        index = sum % self.capacity()
+        while self.lyst[index] is not None:
+            if self.lyst[index][0] == key:
+                self.lyst[index][1] = value
+                break
+            index += 1
+            if index == len(self.lyst):
+                index -= len(self.lyst)
+        if self.lyst[index] is None:
+            self.lyst[index] = [key, value]
+            self.num_keys += 1
+        load_factor = self.num_keys/self.capacity()
+        if load_factor > self.max_loadfactor:
+            self.rehash()
+
+    def clear(self):
+        ''' emptys the hashmap '''
+        self.lyst = [None] * 8
+        self.num_keys = 0
+
+    def capacity(self):
+        ''' returns the current capacity, number of buckets '''
+        return len(self.lyst)
+
+    def size(self):
+        ''' returns the number of key-value pairs in the map '''
+        return self.num_keys
+
+    def keys(self):
+        ''' returns a list of keys '''
+        lyst_of_keys = []
+        for pair in self.lyst:
+            if pair is not None:
+                key = pair[0]
+                lyst_of_keys.append(key)
+        return lyst_of_keys
+
+    def pairs(self):
+        ''' returns a list of keys '''
+        lyst_of_pairs = []
+        for pair in self.lyst:
+            if pair is not None:
+                lyst_of_pairs.append(pair)
+        return lyst_of_pairs
+
+    def rehash(self):
+        ''' rebuild the table that is twice the capacity of the current table '''
+        all_pairs = self.pairs()
+        self.lyst = [None]*(self.capacity()*2)
+        self.num_keys = 0
+        for pair in all_pairs:
+            self.set(pair[0], pair[1])
---
+++ main.py
@@ -0,0 +1,35 @@
+''' main.py for project 7 - dictionaries '''
+
+from hashmap import HashMap
+
+def clean_line(raw_line):
+    ''' removes all punctuation and return a list of all words with a length greater than one '''
+    if not isinstance(raw_line, str):
+        raise ValueError("Input must be a string")
+    line = raw_line.strip().lower()
+    line = list(line)
+    for index in range(len(line)):
+        if line[index] < 'a' or line[index] > 'z':
+            line[index] = ' '
+    cleaned = ''.join(line)
+    words = [word for word in cleaned.split() if len(word) > 1]
+    return words
+
+def main():
+    ''' driver for project '''
+    hash_map = HashMap()
+    with open("AliceInWonderland.txt", encoding='utf8') as input_file:
+        for raw_line in input_file:
+            words = clean_line(raw_line)
+            for word in words:
+                hash_map.set(word, hash_map.get(word, 0)+1)
+    keys = hash_map.keys()
+    key_values = [(key, hash_map.get(key)) for key in keys]
+    key_values.sort(reverse=True, key=lambda x: x[1])
+
+    print("The most common words are:")
+    for element in key_values[:15]:
+        print(f'{element[0]}\t\t{element[1]}')
+    print()
+
+main()
